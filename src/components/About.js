d3679fd64209f30dc708313ee982b301998612b6
MurituMaina <vincent.maina@student.moringaschool.com>
2022-09-21 17:33:07 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+
+
+// console.log(user.links.linkedin)
+// console.log(user.links.github)
+
+function About(user) {
+// console.log(props)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {user.bio ?<p>{user.bio}</p>: ""}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links linkedin={user.linkedin} github={user.github} />
+      
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import user from "../data/user";
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+// console.log(user.links.linkedin)
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
-    </div>
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About bio={user.bio} linkedin={user.links.linkedin} github={user.links.github}/>
+      </div>
   );
 }
 
